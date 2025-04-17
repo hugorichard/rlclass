ac459f759d587e34d0f7234c89b0e58a227fd5ce
hugorichard <research.hugo.richard@gmail.com>
2025-04-17 15:53:42 +0200

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="rlclass",
-    version="0.0.4",
+    version="0.0.5",
     author="Hugo RICHARD",
     author_email="research.hugo.richard@gmail.com",
     description="",
